e4a120fdd168f05f7f83bfe51eb4bf771bdc8e6e
layitf <13658025057@163.com>
2023-07-05 00:00:54 +0800

    feat：file deal

--- react-webpack-demo/webpack.prod.config.js
+++ react-webpack-demo/webpack.prod.config.js
@@ -7,10 +7,15 @@ const ParallelUglifyPlugin = require('webpack-parallel-uglify-plugin');
 
 module.exports = merge(base, {
     plugins: [
+        /**
+         * 生产使用
+         * 不传入任何参数，该插件会默认使用 `output.path` 目录(一般为dist)作为需要清空的目录
+         * 配置 option 使用较少
+         * @see https://github.com/johnagan/clean-webpack-plugin
+         */
         new CleanWebpackPlugin(),
         new ParallelUglifyPlugin({
-            // 传递给 UglifyJS 的参数
-            // （还是使用 UglifyJS 压缩，只不过帮助开启了多进程）
+            // 传递给 UglifyJS 的参数, 使用 UglifyJS 压缩，只不过帮助开启了多进程
             uglifyJS: {
                 output: {
                     beautify: false, // 最紧凑的输出
